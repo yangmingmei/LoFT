95043a5eee5afbf305e79ac78deb7f8967f8de47
Mei MY <67690781+yangmingmei@users.noreply.github.com>
2024-09-12 21:20:55 +0800

    update documentation and examples

--- Example1.m
+++ Example1.m
@@ -14,37 +14,19 @@ U0 = 12;         % mean wind speed
 I0 = 5;         % tubulence intensity
 Turbwind = GenerateTurb5(U0,I0);
 
-figure
-set(gcf,'unit','centimeters','position',[10 5 40 10]); % 10cm*20cm
-plot(Turbwind.WT1.Time, Turbwind.WT1.WindVelX)
-hold on 
-plot(Turbwind.WT2.Time, Turbwind.WT2.WindVelX)
-plot(Turbwind.WT3.Time, Turbwind.WT3.WindVelX)
-plot(Turbwind.WT4.Time, Turbwind.WT4.WindVelX)
-plot(Turbwind.WT5.Time, Turbwind.WT5.WindVelX)
-% plotWindsWaves
-
 %% Generate wave disturbances
+% This part will be released after the paper is acccepted
 
 
-
-
-
-%% Generate Mooring Matrix (in different water depth)
-
-
-% Create_Mooring_Matrix()
+%% Generate Mooring Matrix (in different water depth，mooring length and archor radius)
+% This part will be released after the paper is acccepted
 
 
 %% Communication Failures 
 % Communication failures will block the command from onshore centralized controllers
-
-% A random communication failure
-Communication = randn([5,1]);
-Communication = double( Communication > min(Communication) );
-
 % We don't involve a centralized controller in this example. So the local
 % controller is adopted.
+
 Communication = [0,0,0,0,0];
 
 %% Conduct simulations and save results
@@ -55,5 +37,6 @@ simu.dt = 0.025;
 Out  = sim(mdl,'StopTime',num2str(500));
 save('Results\Example1.mat',"Out");
 
+%% Plot and Visualize
 
 
